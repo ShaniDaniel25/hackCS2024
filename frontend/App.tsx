cc6f45b07a06f19ede40e6add8ab296802db3321
Shani Daniel <shanidaniel1@mail,tau.ac.il>
2024-07-26 02:43:14 +0300

    shaniniz

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,13 +4,17 @@ import MainPage from './screens/MainPage';
 import { createStackNavigator } from '@react-navigation/stack';
 import RedButtonScreen from './screens/RedButtonScreen';
 import ToolsScreen from './screens/ToolsScreen';
+import LoginScreen from './screens/LoginScreen';
+import MoreInfoScreen from './screens/MoreInfoScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainPage">
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="MoreInfo" component={MoreInfoScreen} options={{ headerShown: false }} />
         <Stack.Screen name="MainPage" component={MainPage} options={{ headerShown: false }} />
         <Stack.Screen name="RedButton" component={RedButtonScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Tools" component={ToolsScreen} options={{ headerShown: false }} />
